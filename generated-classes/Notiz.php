03e9dd9bfc189a6b640a2964a5c27d53201db502
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-11-27 08:41:20 +0100

    Added a constructor for Notiz class and setBetreff method

--- generated-classes/Notiz.php
+++ generated-classes/Notiz.php
@@ -15,4 +15,34 @@ use Base\Notiz as BaseNotiz;
 class Notiz extends BaseNotiz
 {
 
+    /**
+     * @param Person $besitzer Besitzer der Notiz
+     * @param string $betreff Betreff der Notiz
+     * @param string $text Text der Notiz
+     */
+    public function __construct(Person $besitzer, $betreff, $text) {
+        parent::__construct();
+        if(is_string($betreff) && is_string($text)) {
+            $this->setBesitzer($besitzer);
+            $this->setBetreff($betreff);
+            $this->setText($text);
+        }else {
+            throw new InvalidArgumentException("betreff und text muessen strings sein");
+        }
+    }
+
+    /**
+     * @see parent::setBetreff()
+     */
+    public function setBetreff($betreff) {
+        if(is_string($betreff)) {
+            if(strlen($betreff) <= 100) {
+                parent::setBetreff($betreff);
+            }else {
+                throw new InvalidArgumentException("betreff darf max. 100 Zeichen lang sein");
+            }
+        }else {
+            throw new InvalidArgumentException("betreff muss ein string sein");
+        }
+    }
 }
